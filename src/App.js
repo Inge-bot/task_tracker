ea8583d0ea5598c8067baa0633615d20c033d3de
IngeBot <ingebot@github.com>
2021-10-10 14:26:46 +0200

    Add react icons, Task component, delete function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from "react"
 
-function App({color, text}) {
+// can also use context api or redux for global state
+function App({ color, text }) {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Dentist appointment',
+      day: 'Feb 3 at 10:45',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 12:00',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Grocery shopping',
+      day: 'Feb 14th at 13:00',
+      reminder: false,
+    }
+  ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
   return (
     <div className='container'>
-     <Header/>
-     <Tasks/>
+      <Header />
+     {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No tasks to do'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,49 +1,13 @@
-import { useState } from "react"
-// const tasks = [
-//     {
-//         id: 1,
-//         text: 'Dentist appointment',
-//         day: 'Feb 3 at 10:45',
-//         reminder: true,
-//     },
-//     {
-//         id: 2,
-//         text: 'Meeting at School',
-//         day: 'Feb 6th at 12:00',
-//         reminder: true,
-//     },
-//     {
-//         id: 3,
-//         text: 'Grocery shopping',
-//         day: 'Feb 14th at 13:00',
-//         reminder: false,
-//     }
-// ]
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Dentist appointment',
-            day: 'Feb 3 at 10:45',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Meeting at School',
-            day: 'Feb 6th at 12:00',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Grocery shopping',
-            day: 'Feb 14th at 13:00',
-            reminder: false,
-        }
-    ])
+import Task from './Task'
+
+
+const Tasks = ({ tasks, onDelete }) => {
+
     return (
         <>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>
+                <Task key={task.id} task={task} 
+                onDelete={onDelete}/>
             ))}
         </>
     )
