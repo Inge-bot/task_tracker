9c2b8a1c68589de1558c4dc126746b7a1d3ed769
IngeBot <ingebot@github.com>
2021-10-11 08:59:23 +0200

    Add toggle state for Add button component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddTask from './components/AddTask'
 
 // can also use context api or redux for global state
 function App({ color, text }) {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -30,7 +31,9 @@ function App({ color, text }) {
 
   // Add Task
   const addTask = (task) => {
-    console.log(task)
+    const id = Math.floor(Math.random() * 1000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
   }
 
   // Delete Task
@@ -52,8 +55,8 @@ function App({ color, text }) {
 
   return (
     <div className='container'>
-      <Header />
-      <AddTask onAdd={addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No tasks to do'}
     </div>
   );
